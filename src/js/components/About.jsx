898aba25b159abf36ed49aa48d0b8160b2a47016
Shafik Bahou <snb3000@gmail.com>
2024-07-18 01:46:21 +0000

    made styles changes

--- src/js/components/About.jsx
+++ src/js/components/About.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const About = () => {
   return (
-    <section id="about">
+    <section id="about" style={{ textAlign: "center" }}>
       <h2>About Us</h2>
       <p>
         Welcome to our website. We are dedicated to providing the best service
--- src/js/components/Header.jsx
+++ src/js/components/Header.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 
 const Header = () => (
-  <header className="py-5 bg-light">
+  <header className="py-5 bg-secondary">
     <div className="container">
       <div className="row justify-content-center">
         <div className="col-lg-8 col-xxl-6">
           <div className="text-center my-5">
             <h1 className="fw-bolder">A Warm Welcome!</h1>
-            <p className="lead fw-normal text-muted mb-5">
+            <p className="lead fw-normal text-white mb-5">
               Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa,
               ipsam, eligendi, in quo sunt possimus non incidunt odit vero
               aliquid similique quaerat nam nobis illo aspernatur vitae fugiat
